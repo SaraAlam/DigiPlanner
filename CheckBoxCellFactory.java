cb047d4e922cedaf0db631334a8491663ec236c0
Diego Abanto <dabantoibarguen@colgate.edu>
2023-05-04 15:03:29 -0400

    Delete class

--- CheckBoxCellFactory.java
+++
@@ -1,8 +0,0 @@
-/* 
-public class CheckBoxCellFactory implements Callback {
-    @Override
-    public TableCell call(Object param) {
-        CheckBoxTableCell<Boolean> checkBoxCell = new CheckBoxTableCell();
-        return checkBoxCell;
-    }
-} */
